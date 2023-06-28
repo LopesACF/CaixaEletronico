d8be01b40ba37a58298ef13568ac1e71f1406ae9
Altemilson Lopes <altemilsonlopes@gmail.com>
2023-06-27 23:39:26 -0300

    Finalidado

--- Program.cs
+++ Program.cs
@@ -1,42 +1,40 @@
 ﻿Console.Clear();
 
-void dialog(string mensagem)
-{
-    Console.WriteLine(mensagem);
-}
+cyan();
+dialog("--- Caixa Eletrônico ---", 75);
+reset();
 
 yellow();
-dialog("--- Caixa Eletrônico ---");
+dialog($"\nNotas disponíveis para saque: 1, 2, 5, 10, 20, 50, 100 e 200 reais", 50);
 reset();
 
 blue();
-dialog("\nNotas disponíveis para saque: 1, 2, 5, 10, 20, 50, 100 e 200 reais");
-
-dialog("\nDigite o valor a ser sacado...: ");
+dialog("\nDigite o valor a ser sacado...: ", 50);
 reset();
 
-red();
+green();
 string valueA = Console.ReadLine()!;
 
 if (!int.TryParse(valueA, out int value) || (value == 0 && valueA.Length == 1) || valueA.Contains(","))
 {
     
-    dialog("\nValor não pode ser 0 e não pode conter centavos!!");
+    dialog("\nValor não pode ser 0 e não pode conter centavos!!", 50);
     reset();
 } 
 else
 {
-    int[] grade = { 200, 100, 50, 20, 10, 5, 2, 1 };
+    int[] grade = {200, 100, 50, 20, 10, 5, 2, 1 };
 
     yellow();
-    dialog("\nO caixa deve entregar:");
-
+    dialog("\nO caixa deve entregar: \n", 50);
+    
+    green();
     foreach (int note in grade)
     {
         int quantidadegrade = value / note;
         if (quantidadegrade > 0)
         {
-            dialog($"{quantidadegrade} nota(s) de R${note}");
+            dialog($"\n{quantidadegrade} nota(s) de R${note}", 75);
             
 
             value %= note;
@@ -45,19 +43,33 @@ else
 }
 reset();
 
+void dialog(string texto, int intervalo)
+{
+    foreach (char c in texto)
+    {
+        Console.Write(c);
+        Thread.Sleep(intervalo);
+    }
+}
+
 void reset()
 {
     Console.ResetColor();
 }
 
+void green()
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+}
+
+void cyan()
+{
+    Console.ForegroundColor = ConsoleColor.Cyan;
+}
+
 void blue()
 {
     Console.ForegroundColor = ConsoleColor.DarkBlue;
-}
-
-void red()
-{
-    Console.ForegroundColor = ConsoleColor.DarkRed;
 }
 
 void yellow()
